a927fdc5b984ac3dbde1232a35fbf06d2dd0dfc8
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-07 10:38:16 +0600

    Добавлены zeroValues в getCurrentSumGenerator

--- stake_info/getCurrentSum.ts
+++ stake_info/getCurrentSum.ts
@@ -2,13 +2,17 @@ import { log } from '@kot-shrodingera-team/germes-utils';
 
 const getCurrentSumGenerator = (options: {
   sumInput: string;
+  zeroValues?: string[];
 }) => (): number => {
   const sumInput = document.querySelector(options.sumInput) as HTMLInputElement;
   if (!sumInput) {
     log('Не найдено поле ввода суммы ставки', 'crimson');
     return 0;
   }
-  const sumText = sumInput.value;
+  const sumText = sumInput.value.trim();
+  if (options.zeroValues && options.zeroValues.includes(sumText)) {
+    return 0;
+  }
   const sum = Number(sumText);
   if (Number.isNaN(sum)) {
     log(`Непонятный формат текущей суммы ставки: "${sumText}"`, 'crimson');
