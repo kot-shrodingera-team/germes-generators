b84affe8671aaf1ed8faa2eebc28345107954d9f
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-09 16:49:24 +0600

    Исправлен прототип функции initializeGenerator

--- initialization/initialize.ts
+++ initialization/initialize.ts
@@ -1,36 +1,36 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
 
-const initializeGenerator = (
-  authCheckReady: () => Promise<void>,
-  checkAuth: (timeout?: number) => boolean,
-  balanceReady: () => Promise<boolean>,
-  updateBalance: () => void,
-  authorize: () => Promise<void>,
-  ifLoginOk?: () => Promise<void>
-) => async (): Promise<void> => {
+const initializeGenerator = (options: {
+  authCheckReady: (timeout?: number) => Promise<void>;
+  checkAuth: () => boolean;
+  balanceReady: () => Promise<boolean>;
+  updateBalance: () => void;
+  authorize: () => Promise<void>;
+  ifLoginOk?: () => Promise<void>;
+}) => async (): Promise<void> => {
   if (worker.LoginTry > 3) {
     log('Превышен лимит попыток авторизации', 'crimson');
     return;
   }
 
-  await authCheckReady();
-  worker.Islogin = checkAuth();
+  await options.authCheckReady();
+  worker.Islogin = options.checkAuth();
   worker.JSLogined();
   if (worker.Islogin) {
     log('Есть авторизация', 'green');
     worker.Islogin = true;
     worker.JSLogined();
-    const balanceLoaded = await balanceReady();
+    const balanceLoaded = await options.balanceReady();
     if (!balanceLoaded) {
       log(`Баланс не появился`, 'crimson');
     } else {
-      updateBalance();
+      options.updateBalance();
     }
-    if (ifLoginOk) {
-      ifLoginOk();
+    if (options.ifLoginOk) {
+      options.ifLoginOk();
     }
   } else {
-    authorize();
+    options.authorize();
   }
 };
 
