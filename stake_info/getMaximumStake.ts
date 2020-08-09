6d8ffe7cc0da6a4aa50ab54ae60203508aced42b
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-09 22:42:35 +0600

    Добавлена регулярные выражения в генераторы лимитов

--- stake_info/getMaximumStake.ts
+++ stake_info/getMaximumStake.ts
@@ -1,7 +1,11 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
 
+const defaultMaxRegex = /(\d+(?:\.\d+)?)/;
+const removeRegex = /[\s,']/g;
+
 const getMaximumStakeGenerator = (options: {
   maximumStakeElementSelector: string;
+  maxRegex?: RegExp;
 }) => (): number => {
   const maximumStakeElement = document.querySelector(
     options.maximumStakeElementSelector
@@ -10,16 +14,19 @@ const getMaximumStakeGenerator = (options: {
     log('Не найдена максимальная сумма ставки', 'crimson');
     return 0;
   }
-  const maximumStakeText = maximumStakeElement.textContent.trim();
-  const maximumStake = Number(maximumStakeText);
-  if (Number.isNaN(maximumStake)) {
+  const maximumStakeText = maximumStakeElement.textContent
+    .trim()
+    .replace(removeRegex, '');
+  const maxRegex = options.maxRegex ? options.maxRegex : defaultMaxRegex;
+  const maxMatch = maximumStakeText.match(maxRegex);
+  if (!maxMatch) {
     log(
       `Непонятный формат максимальной ставки: "${maximumStakeText}"`,
       'crimson'
     );
     return 0;
   }
-  return maximumStake;
+  return Number(maxMatch[1]);
 };
 
 export default getMaximumStakeGenerator;
--- stake_info/getMinimumStake.ts
+++ stake_info/getMinimumStake.ts
@@ -1,7 +1,11 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
 
+const defaultMinRegex = /(\d+(?:\.\d+)?)/;
+const removeRegex = /[\s,']/g;
+
 const getMinimumStakeGenerator = (options: {
   minimumStakeElementSelector: string;
+  minRegex?: RegExp;
 }) => (): number => {
   const minimumStakeElement = document.querySelector(
     options.minimumStakeElementSelector
@@ -10,16 +14,19 @@ const getMinimumStakeGenerator = (options: {
     log('Не найдена минимальная сумма ставки', 'crimson');
     return 0;
   }
-  const minimumStakeText = minimumStakeElement.textContent.trim();
-  const minimumStake = Number(minimumStakeText);
-  if (Number.isNaN(minimumStake)) {
+  const minimumStakeText = minimumStakeElement.textContent
+    .trim()
+    .replace(removeRegex, '');
+  const minRegex = options.minRegex ? options.minRegex : defaultMinRegex;
+  const minMatch = minimumStakeText.match(minRegex);
+  if (!minMatch) {
     log(
-      `Непонятный формат минимальной ставки: "${minimumStakeText}"`,
+      `Непонятный формат маинимальной ставки: "${minimumStakeText}"`,
       'crimson'
     );
     return 0;
   }
-  return minimumStake;
+  return Number(minMatch[1]);
 };
 
 export default getMinimumStakeGenerator;
