a9bbfb57f344a43fae6add497b7c1ed1b7108c75
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-10 18:32:51 +0600

    Перемещён authCheckReadyGenerator

--- stake_info/checkAuth.ts
+++ stake_info/checkAuth.ts
@@ -1,3 +1,15 @@
+import { getElement } from '@kot-shrodingera-team/germes-utils';
+
+export const authCheckReadyGenerator = (options: {
+  authFormSelector: string;
+  accountSelector: string;
+}) => async (timeout = 5000): Promise<void> => {
+  await Promise.race([
+    getElement(options.authFormSelector, timeout),
+    getElement(options.accountSelector, timeout),
+  ]);
+};
+
 const checkAuthGenerator = (options: {
   accountSelector: string;
 }) => (): boolean => {
