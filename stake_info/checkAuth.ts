2fa6290916c449af2dbf9026477c1cfde0294b4f
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-25 23:28:53 +0600

    Рефактор authCheckReadyGenerator, добавление опции ожидания авторизации после появления элемента отсутствия авторизации

--- stake_info/checkAuth.ts
+++ stake_info/checkAuth.ts
@@ -1,19 +1,35 @@
 import { getElement } from '@kot-shrodingera-team/germes-utils';
 
-export const authCheckReadyGenerator = (options: {
-  authFormSelector: string;
-  accountSelector: string;
+export const authStateReadyGenerator = (options: {
+  /**
+   * Селектор элемента отсутствия авторизации, например, кнопка авторизации
+   */
+  noAuthSelector: string;
+  /**
+   * Селектор элемента наличия авторизации, например, кнопка аккаунта или баланс
+   */
+  authSelector: string;
+  /**
+   * Ожидание элемента наличия авторизации после появления элемента отсутствия авторизации
+   */
+  maxDelayAfterNoAuth?: 0;
 }) => async (timeout = 5000): Promise<void> => {
   await Promise.race([
-    getElement(options.authFormSelector, timeout),
-    getElement(options.accountSelector, timeout),
+    getElement(options.noAuthSelector, timeout),
+    getElement(options.authSelector, timeout),
   ]);
+  if (
+    options.maxDelayAfterNoAuth &&
+    document.querySelector(options.noAuthSelector)
+  ) {
+    await getElement(options.authSelector, options.maxDelayAfterNoAuth);
+  }
 };
 
 const checkAuthGenerator = (options: {
-  accountSelector: string;
+  authSelector: string;
 }) => (): boolean => {
-  const accountMenu = document.querySelector(options.accountSelector);
+  const accountMenu = document.querySelector(options.authSelector);
   return Boolean(accountMenu);
 };
 
