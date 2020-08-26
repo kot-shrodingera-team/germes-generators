3696f39ea90a4c36fce6794f75f00aa6ffb1bcee
kot-shrodingera <kot.shrodingera@gmail.com>
2020-08-26 16:15:52 +0600

    Добавлено логгирование в authStateReadyGenerator

--- stake_info/checkAuth.ts
+++ stake_info/checkAuth.ts
@@ -1,35 +1,64 @@
-import { getElement } from '@kot-shrodingera-team/germes-utils';
+import { getElement, log } from '@kot-shrodingera-team/germes-utils';
 
 export const authStateReadyGenerator = (options: {
   /**
    * Селектор элемента отсутствия авторизации, например, кнопка авторизации
    */
-  noAuthSelector: string;
+  noAuthElementSelector: string;
   /**
    * Селектор элемента наличия авторизации, например, кнопка аккаунта или баланс
    */
-  authSelector: string;
+  authElementSelector: string;
   /**
    * Ожидание элемента наличия авторизации после появления элемента отсутствия авторизации
    */
-  maxDelayAfterNoAuth?: number;
+  maxDelayAfterNoAuthElementAppeared?: number;
+  /**
+   * Включение логгирования
+   */
+  logging: boolean;
 }) => async (timeout = 5000): Promise<void> => {
   await Promise.race([
-    getElement(options.noAuthSelector, timeout),
-    getElement(options.authSelector, timeout),
+    getElement(options.noAuthElementSelector, timeout),
+    getElement(options.authElementSelector, timeout),
   ]);
-  if (
-    options.maxDelayAfterNoAuth &&
-    document.querySelector(options.noAuthSelector)
-  ) {
-    await getElement(options.authSelector, options.maxDelayAfterNoAuth);
+  const noAuthElement = document.querySelector(options.noAuthElementSelector);
+  const authElement = document.querySelector(options.authElementSelector);
+  if (options.maxDelayAfterNoAuthElementAppeared && noAuthElement) {
+    if (options.logging) {
+      log(
+        `Появился элемент отсутсвия авторизации, ожидаем элемент наличия авторизации`,
+        'steelblue'
+      );
+    }
+    const authElementWaited = await getElement(
+      options.authElementSelector,
+      options.maxDelayAfterNoAuthElementAppeared
+    );
+    if (options.logging) {
+      if (authElementWaited) {
+        log(`Появился элемент наличия авторизации`, 'steelblue');
+      } else {
+        log(`Элемент наличия авторизации не появился`, 'steelblue');
+      }
+    }
+    return;
+  }
+  if (options.logging) {
+    if (noAuthElement) {
+      log(`Появился элемент отсутствия авторизации`, 'steelblue');
+    } else if (authElement) {
+      log(`Появился элемент наличия авторизации`, 'steelblue');
+    } else {
+      log(`Не найден элемент наличия или отсутствия авторизации`, 'steelblue');
+    }
   }
 };
 
 const checkAuthGenerator = (options: {
-  authSelector: string;
+  authElementSelector: string;
 }) => (): boolean => {
-  const accountMenu = document.querySelector(options.authSelector);
+  const accountMenu = document.querySelector(options.authElementSelector);
   return Boolean(accountMenu);
 };
 
