7a8fa0967de674cb0e3e7fc9172d4b4aa14c4d3d
kot-shrodingera <kot.shrodingera@gmail.com>
2020-10-20 23:51:34 +0600

    feat(getCoefficient): опциональная функция получения текста коэффициента

--- stake_info/getCoefficient.ts
+++ stake_info/getCoefficient.ts
@@ -2,15 +2,23 @@ import { log } from '@kot-shrodingera-team/germes-utils';
 
 const getCoefficientGenerator = (options: {
   coefficientSelector: string;
+  getCoefficientText?: () => string;
   context?: Document | Element;
 }) => (): number => {
-  const context = options.context ? options.context : document;
-  const coefficientElement = context.querySelector(options.coefficientSelector);
-  if (!coefficientElement) {
-    log('Коэффициент не найден', 'crimson');
-    return 0;
-  }
-  const coefficientText = coefficientElement.textContent.trim();
+  const coefficientText = (() => {
+    if (options.getCoefficientText) {
+      return options.getCoefficientText();
+    }
+    const context = options.context ? options.context : document;
+    const coefficientElement = context.querySelector(
+      options.coefficientSelector
+    );
+    if (!coefficientElement) {
+      log('Коэффициент не найден', 'crimson');
+      return 0;
+    }
+    return coefficientElement.textContent.trim();
+  })();
   const coefficient = Number(coefficientText);
   if (Number.isNaN(coefficient)) {
     log(`Непонятный формат коэффициента: "${coefficientText}"`, 'crimson');
