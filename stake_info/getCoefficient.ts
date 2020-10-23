a9314b323dfb4c9253fce07c28bdecb9e91f02ec
kot-shrodingera <kot.shrodingera@gmail.com>
2020-10-23 07:10:54 +0600

    feat(getCoefficient): опциональный regex

--- stake_info/getCoefficient.ts
+++ stake_info/getCoefficient.ts
@@ -1,29 +1,38 @@
 import { log } from '@kot-shrodingera-team/germes-utils';
 
+const defaultCoefficientRegex = /(\d+(?:\.\d+)?)/;
+
 const getCoefficientGenerator = (options: {
   coefficientSelector: string;
   getCoefficientText?: () => string;
+  coefficientRegex?: RegExp;
   context?: Document | Element;
 }) => (): number => {
-  const coefficientText = (() => {
-    if (options.getCoefficientText) {
-      return options.getCoefficientText();
-    }
-    const context = options.context ? options.context : document;
-    const coefficientElement = context.querySelector(
-      options.coefficientSelector
-    );
-    if (!coefficientElement) {
-      log('Коэффициент не найден', 'crimson');
+  if (options.getCoefficientText) {
+    const coefficientText = options.getCoefficientText();
+    const coefficient = Number(coefficientText);
+    if (Number.isNaN(coefficient)) {
+      log(`Непонятный формат коэффициента: "${coefficientText}"`, 'crimson');
       return 0;
     }
-    return coefficientElement.textContent.trim();
-  })();
-  const coefficient = Number(coefficientText);
-  if (Number.isNaN(coefficient)) {
+    return coefficient;
+  }
+  const context = options.context ? options.context : document;
+  const coefficientElement = context.querySelector(options.coefficientSelector);
+  if (!coefficientElement) {
+    log('Коэффициент не найден', 'crimson');
+    return 0;
+  }
+  const coefficientText = coefficientElement.textContent.trim();
+  const coefficientRegex = options.coefficientRegex
+    ? options.coefficientRegex
+    : defaultCoefficientRegex;
+  const coefficientMatch = coefficientText.match(coefficientRegex);
+  if (!coefficientMatch) {
     log(`Непонятный формат коэффициента: "${coefficientText}"`, 'crimson');
     return 0;
   }
+  const coefficient = Number(coefficientMatch[1]);
   return coefficient;
 };
 
