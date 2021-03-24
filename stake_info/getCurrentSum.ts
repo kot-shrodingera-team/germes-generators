88bd5a18cff94bec4818b091da69f14690efd373
kot-shrodingera <kot.shrodingera@gmail.com>
2021-03-24 22:03:04 +0600

    feat(getCurrentSum): добавление опций replaceDataArray и removeRegex

--- stake_info/getCurrentSum.ts
+++ stake_info/getCurrentSum.ts
@@ -4,6 +4,11 @@ import { defaultNumberRegex, defaultRemoveRegex } from './defaultRegexes';
 const getCurrentSumGenerator = (options: {
   sumInput: string;
   zeroValues?: string[];
+  replaceDataArray?: {
+    searchValue: string | RegExp;
+    replaceValue: string;
+  }[];
+  removeRegex?: RegExp;
   currentSumRegex?: RegExp;
   context?: () => Document | Element;
 }) => (): number => {
@@ -13,13 +18,25 @@ const getCurrentSumGenerator = (options: {
     log('Не найдено поле ввода суммы ставки', 'crimson');
     return 0;
   }
-  const sumText = sumInput.value.trim().replace(removeRegex, '');
+  let sumText = sumInputElement.value.trim();
   if (sumText === '') {
     return 0;
   }
   if (options.zeroValues && options.zeroValues.includes(sumText)) {
     return 0;
   }
+  if (options.replaceDataArray) {
+    options.replaceDataArray.forEach((replaceData) => {
+      sumText = sumText.replace(
+        replaceData.searchValue,
+        replaceData.replaceValue
+      );
+    });
+  }
+  const removeRegex = options.removeRegex
+    ? options.removeRegex
+    : defaultRemoveRegex;
+  sumText = sumText.replace(removeRegex, '');
   const currentSumRegex = options.currentSumRegex
     ? options.currentSumRegex
     : defaultNumberRegex;
